c01e6c875346b3cdd67ce21d54f073a73a79f271
alejandronieto-dam <alejandrona5b@gmail.com>
2024-05-22 16:47:10 +0200

    First commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,12 @@
+# CMake generated Testfile for 
+# Source directory: /home/nietoff/tfg/src
+# Build directory: /home/nietoff/tfg/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("turtlebot3_machine_learning-master/turtlebot3_machine_learning")
+subdirs("turtlebot3_simulations-master/turtlebot3_simulations")
+subdirs("turtlebot3_machine_learning-master/turtlebot3_dqn")
+subdirs("turtlebot3_simulations-master/turtlebot3_fake")
+subdirs("turtlebot3_simulations-master/turtlebot3_gazebo")
---
+++ build/turtlebot3_machine_learning-master/turtlebot3_dqn/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/nietoff/tfg/src/turtlebot3_machine_learning-master/turtlebot3_dqn
+# Build directory: /home/nietoff/tfg/build/turtlebot3_machine_learning-master/turtlebot3_dqn
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
